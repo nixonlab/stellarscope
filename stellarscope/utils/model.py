fix underflow in csr_matrix_plus.multiply and TelescopeLikelihood.calculate_lnl()

stellarscope/utils/model.py
@@ -764,9 +764,15 @@ class TelescopeLikelihood(object):
 
         """
         lg.debug('CALL: TelescopeLikelihood.calculate_lnl()')
-        _amb = self.Q.multiply(self.Y).multiply(pi * theta)
-        _uni = self.Q.multiply(1 - self.Y).multiply(pi)
-        _inner = csr_matrix(_amb + _uni)
+        try:
+            _amb = self.Q.multiply(self.Y).multiply(pi * theta)
+            _uni = self.Q.multiply(1 - self.Y).multiply(pi)
+            _inner = csr_matrix(_amb + _uni)
+        except FloatingPointError:
+            lg.debug('using extended precision')
+            _amb = self.Q.astype(np.longdouble).multiply(self.Y).multiply(pi * theta)
+            _uni = self.Q.astype(np.longdouble).multiply(1 - self.Y).multiply(pi)
+            _inner = csr_matrix(_amb + _uni)
         cur = z.multiply(_inner.log1p()).sum()
         lg.debug('EXIT: TelescopeLikelihood.calculate_lnl()')
         return cur

stellarscope/utils/sparse_plus.py
@@ -14,6 +14,8 @@ import numpy as np
 import scipy.sparse
 from numpy.random import default_rng
 
+import logging as lg
+
 __author__ = 'Matthew L. Bendall'
 __copyright__ = "Copyright (C) 2019 Matthew L. Bendall"
 
@@ -200,7 +202,12 @@ class csr_matrix_plus(scipy.sparse.csr_matrix):
         return ret
 
     def multiply(self, other):
-        return type(self)(super().multiply(other))
+        try:
+            return type(self)(super().multiply(other))
+        except FloatingPointError:
+            lg.info('using extended precision')
+            longcopy = scipy.sparse.csr_matrix(self).astype(np.longdouble)
+            return type(self)(longcopy.multiply(other))
 
 
     def colsums(self) -> csr_matrix_plus: